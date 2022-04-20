799f8bf25be83c8fea1d21f391cf61251ee9bc49
Peter LaValle <peter.lavalle@gmail.com>
2022-04-20 14:29:19 +0100

    publish

---
+++ boxes.hs
@@ -0,0 +1,21 @@
+
+
+Army Box
+- Core
+- HQ
+- Elite
+- Elite
+
+Combat Patrol
+- Core
+- HQ
+- Elite
+- Transport
+
+
+|-------------|--------|HQ |Elite|Core|Heavy|Transport
+|Black Templar|        |
+|             |Army Box|2  |1    |1   |1    |
+|             |Patrol  |1  |0    |1/2 |0    |1
+|Beast Snaggas|
+|Sisters
